8a9952b51f814c53761bb3107c631f4f26a08314
sethvincent <sethvincent@gmail.com>
2012-12-08 14:45:22 -0800

    fix links on frontpage

--- site/wp-content/themes/nmedc/frontpage.php
+++ site/wp-content/themes/nmedc/frontpage.php
@@ -22,25 +22,25 @@ Template Name: Front page
       <div class="home-teaser">
         <h5 class="green-bg">Advocacy & Support</h5>
         <p>Starting point for new and existing businesses who need guidance and support.</p>
-        <a href="" class="read-more">Read More</a>
+        <a href="/business-advocacy/" class="read-more">Read More</a>
       </div>
     
       <div class="home-teaser">
         <h5 class="green-bg">Promotion of the Region</h5>
         <p>Creating awareness of New Milford's vibrant economic and cultural community.</p>
-        <a href="" class="read-more">Read More</a>
+        <a href="/important-resources/" class="read-more">Read More</a>
       </div>
     
       <div class="home-teaser">
         <h5 class="green-bg">Financial Assistance</h5>
         <p>Development, underwriting, and disbursement of financial assistance to local businesses.</p>
-        <a href="" class="read-more">Read More</a>
+        <a href="/financial-assistace/" class="read-more">Read More</a>
       </div>
     
       <div class="home-teaser">
         <h5 class="green-bg">Why New Milford?</h5>
         <p>Learn about why you should choose New Milford as a place to build your business.</p>
-        <a href="" class="read-more">Read More</a>
+        <a href="/why-new-milford/" class="read-more">Read More</a>
       </div>
     </div>
     
